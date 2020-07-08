7d9d579742793b9dae4348faa81a9fa8dcd2cfa8
wangwang <572772828@qq.com>
2020-07-08 16:30:51 +0800

    spring cloud config & nacos & kafka

--- config-client/src/main/java/com/diyiliu/cloud/config/nacos/client/controller/TestController.java
+++ config-client/src/main/java/com/diyiliu/cloud/config/nacos/client/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.diyiliu.cloud.config.bus.client.controller;
+package com.diyiliu.cloud.config.nacos.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
